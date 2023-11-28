a1539b08b594996ed8de381d85685890e098b08d
Kevin <ckzhang1026@gmail.com>
2023-11-27 17:47:51 -0800

    Style changes to home page

--- net/lib/pages/home.dart
+++ net/lib/pages/home.dart
@@ -58,116 +58,148 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Config.yellow,
         appBar: Gui.header("Welcome", true),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.topLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Gui.labelButton(
-                      "Logout", 28, () => {checkPrefs(context, true)}),
-                  ElevatedButton(
-                    child: Text('Change Zip Code'),
-                    onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Padding(
-                              padding: EdgeInsets.only(
-                                bottom:
-                                    MediaQuery.of(context).viewInsets.bottom,
-                              ),
-                              child: Container(
-                                  height: 200,
-                                  color: Colors.amber,
-                                  child: Center(
-                                      child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: <Widget>[
-                                      const Text('Change Zip Code'),
-                                      Gui.textInput(
-                                          "Zip Code", zipCodeController),
-                                      ElevatedButton(
-                                        child: const Text('Submit'),
-                                        onPressed: () => {
-                                          Navigator.pop(context),
-                                          Provider.of<ZipCode>(context,
-                                                  listen: false)
-                                              .updateValue(
-                                                  zipCodeController.text)
-                                        },
-                                      ),
-                                    ],
-                                  ))),
-                            );
-                          });
-                    },
-                  )
-                ],
+            child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Gui.labelButton(
+                        "Logout", 24, () => {checkPrefs(context, true)}),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.zipCode,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        ElevatedButton(
+                          child: Text('Change Zip Code'),
+                          onPressed: () {
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: MediaQuery.of(context)
+                                          .viewInsets
+                                          .bottom,
+                                    ),
+                                    child: Container(
+                                        height: 200,
+                                        color: Config.green,
+                                        child: Center(
+                                            child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            const Text(
+                                              'Change Zip Code',
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 20),
+                                            ),
+                                            Gui.textInput(
+                                                "Zip Code", zipCodeController),
+                                            ElevatedButton(
+                                              style: ButtonStyle(
+                                                  backgroundColor:
+                                                      MaterialStatePropertyAll(
+                                                          Config.yellow)),
+                                              child: const Text('Submit'),
+                                              onPressed: () => {
+                                                Navigator.pop(context),
+                                                Provider.of<ZipCode>(context,
+                                                        listen: false)
+                                                    .updateValue(
+                                                        zipCodeController.text)
+                                              },
+                                            ),
+                                          ],
+                                        ))),
+                                  );
+                                });
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Gui.iconLabelButton("Shelters", const Icon(Icons.night_shelter),
-                    () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SheltersPage(
-                              zipCode: widget.zipCode,
-                            )),
-                  );
-                }),
-                Gui.iconLabelButton("Job Search", const Icon(Icons.work), () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const JobsPage()),
-                  );
-                }),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Gui.iconLabelButton(
-                    "Healthcare", const Icon(Icons.medical_services), () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => HealthCarePage(
-                              zipCode: widget.zipCode,
-                            )),
-                  );
-                }),
-                Gui.iconLabelButton("Veterinary", const Icon(Icons.pets), () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const VeterinaryPage()),
-                  );
-                }),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Gui.iconLabelButton("Map View", const Icon(Icons.location_on),
-                    () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MapPage(
-                              zipCode: widget.zipCode,
-                            )),
-                  );
-                }),
-              ],
-            ),
-          ],
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Gui.iconLabelButton(
+                            "Shelters", const Icon(Icons.night_shelter), () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SheltersPage(
+                                      zipCode: widget.zipCode,
+                                    )),
+                          );
+                        }),
+                        Gui.iconLabelButton(
+                            "Job Search", const Icon(Icons.work), () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const JobsPage()),
+                          );
+                        }),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Gui.iconLabelButton(
+                            "Healthcare", const Icon(Icons.medical_services),
+                            () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HealthCarePage(
+                                      zipCode: widget.zipCode,
+                                    )),
+                          );
+                        }),
+                        Gui.iconLabelButton(
+                            "Veterinary", const Icon(Icons.pets), () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const VeterinaryPage()),
+                          );
+                        }),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Gui.iconLabelButton(
+                            "Map View", const Icon(Icons.location_on), () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MapPage(
+                                      zipCode: widget.zipCode,
+                                    )),
+                          );
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         )));
   }
 }
